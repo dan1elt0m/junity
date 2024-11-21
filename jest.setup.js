ad5b80f080540934a1938ba07c558becf38f9a98
Daniel Tom <daniel.tom@xebia.com>
2024-11-21 14:08:56 +0100

    Remove xhr2

--- jest.setup.js
+++ jest.setup.js
@@ -1,6 +1,5 @@
 import { BroadcastChannel } from 'worker_threads';
 import { ucServer } from './src/__mocks__/ucServer';
-import xhr2 from 'xhr2';
 
 Reflect.set(globalThis, 'BroadcastChannel', BroadcastChannel);
 // global.XMLHttpRequest = xhr2;
