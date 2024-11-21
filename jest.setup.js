0e1a0ef8fffdaedad609b585544fbee602e51a4b
Daniël Tom <d.e.tom89@gmail.com>
2024-11-21 21:32:02 +0800

    Remove xhr2 dep (#14)
    * Remove xhr2 dep

--- jest.setup.js
+++ jest.setup.js
@@ -1,9 +1,7 @@
 import { BroadcastChannel } from 'worker_threads';
 import { ucServer } from './src/__mocks__/ucServer';
-import xhr2 from 'xhr2';
 
 Reflect.set(globalThis, 'BroadcastChannel', BroadcastChannel);
-// global.XMLHttpRequest = xhr2;
 
 beforeAll(() => {
   // Start the interception.
